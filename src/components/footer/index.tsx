6ec209da5fc576781393db401f2776897273011a
Samuel Belo <belo.samuel@gmail.com>
2023-09-07 08:47:55 -0300

    feat: mobile style added to footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -4,9 +4,9 @@ import Container from "../container";
 export default function Footer() {
     return (
         <Container className="bg-slate-300 h-[400px]">
-            <div className="flex flex-col gap-8 justify-center h-full">
-                <h1 className="font-bold text-slate-900 text-2xl leading-6">E-COMMERCE</h1>
-                <div className="w-full flex gap-20">
+            <h1 className="font-bold text-slate-900 text-2xl leading-6">E-COMMERCE</h1>
+            <div className="max-sm:gap-1 flex flex-col gap-8 justify-center items-center h-full">
+                <div className="max-sm:grid max-sm:grid-cols-2 max-sm:gap-1 max-sm:p-4 w-full flex gap-20">
                     <div className="flex flex-col gap-3">
                         <h2 className="text-slate-800 font-semibold text-lg leading-6">Contact Us</h2>
                         <p className="text-slate-700 font-normal text-base leading-6">Lorem Ipsum</p>
